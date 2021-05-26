40c642bb332c5e11f1ad0ef94a49d25871b87926
rahim jangi <rahimjangi1982@yahoo.com>
2021-05-26 20:01:40 +0200

    Web Api
    A blank web api project added.

---
+++ DataManager/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace DataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            string userNane = RequestContext.Principal.Identity.GetUserName();
+            string userType = RequestContext.Principal.Identity.AuthenticationType;
+            return new string[] { userId, userNane, userType };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
