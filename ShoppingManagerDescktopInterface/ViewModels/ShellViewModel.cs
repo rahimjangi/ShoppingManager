9537c068dc37f2ca64568808d17575366d301a63
rahim jangi <rahimjangi1982@yahoo.com>
2021-05-29 10:29:45 +0200

    Login page
    Login page createt and ready to connect to api

--- ShoppingManagerDescktopInterface/ViewModels/ShellViewModel.cs
+++ ShoppingManagerDescktopInterface/ViewModels/ShellViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace ShoppingManagerDescktopInterface.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel:Conductor<object>
     {
+        private readonly LoginViewModel _loginViewModel;
+
+        public ShellViewModel(LoginViewModel loginViewModel)
+        {
+            _loginViewModel = loginViewModel;
+            ActivateItemAsync(_loginViewModel);
+
+        }
     }
 }
